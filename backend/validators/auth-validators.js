787794d89755874faf0c4262798476c0a58fe47f
Achin-Agarwal <achinagarwal9044@gmail.com>
2025-06-06 20:56:47 +0530

    Refactor authentication validation schemas to improve error messaging and enforce stricter validation rules

--- backend/validators/auth-validators.js
+++ backend/validators/auth-validators.js
@@ -2,20 +2,47 @@ import z from "zod";
 
 export const loginUserSchema = z.object({
   email: z
-    .string()
+    .string({ message: "Email is required." })
     .trim()
-    .email({ message: "Please enter a valid email address" })
-    .max(100, { message: "Email must not exceed 100 characters" }),
+    .email({ message: "Please enter a valid email address." })
+    .max(50, { message: "Email must not exceed 50 characters." }),
   password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long" })
-    .max(100, { message: "Password must not exceed 100 characters" }),
+    .string({ message: "Password is required." })
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}|:;<>,.?/~\-=\[\]])[A-Za-z\d!@#$%^&*()_+{}|:;<>,.?/~\-=\[\]]{6,}$/,
+      {
+        message:
+          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+      }
+    ),
 });
 
-export const registerUserSchema = loginUserSchema.extend({
-  name: z
-    .string()
-    .trim()
-    .min(3, { message: "Name must be at least 3 characters long" })
-    .max(50, { message: "Name must not exceed 50 characters" }),
-});
+export const registerUserSchema = loginUserSchema
+  .extend({
+    username: z
+      .string({ message: "Username is required." })
+      .trim()
+      .min(3, { message: "Name must be at least 3 characters long." })
+      .max(50, { message: "Name must not exceed 50 characters." }),
+    confirmPassword: z
+      .string({ message: "Confirm password field is required." })
+      .min(6, {
+        message: "Confirm password must be at least 6 characters long.",
+      })
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}|:;<>,.?/~\-=\[\]])[A-Za-z\d!@#$%^&*()_+{}|:;<>,.?/~\-=\[\]]{6,}$/,
+        {
+          message:
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+        }
+      ),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
+
+export const loginAdminSchema = loginUserSchema;
+
+export const registerAdminSchema = registerUserSchema;
