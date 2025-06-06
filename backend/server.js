e009d8c33a6a1be5224b69dad9ade38b1f909a4b
Achin-Agarwal <achinagarwal9044@gmail.com>
2025-06-06 14:10:14 +0530

    Enhance security and data handling in backend
    - Added middleware for security: helmet, xss-clean, hpp, and express-mongo-sanitize.
    - Updated package.json and package-lock.json with new dependencies.
    - Created upload.js route file for future implementation.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import helmet from "helmet";
+import xss from "xss-clean";
+import hpp from "hpp";
+import mongoSanitize from "express-mongo-sanitize";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(helmet());
+app.use(cors({
+  origin: 'https://yourdomain.com',
+  methods: ['GET', 'POST']
+}));
+app.use(xss());
+app.use(hpp());
+app.use(mongoSanitize());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
